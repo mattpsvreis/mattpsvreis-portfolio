0ec1481fea8d9fb928a1567c214bb3f6d418d991
Mateus Reis <mateuspsvreis@gmail.com>
2023-12-28 15:31:29 -0300

    feat: content theme

---
+++ src/constants/content-theme.ts
@@ -0,0 +1,14 @@
+import CoverImage from './assets/images/cover-image.png';
+import LogoImageLight from './assets/svgs/full-white.svg';
+import loader from './assets/gifs/loader.gif';
+import LogoImage from './assets/svgs/logo.svg';
+import Favicon from './assets/svgs/favicon.svg';
+
+export const CONTENT_THEME = {
+  logo: LogoImage,
+  light_logo: LogoImageLight,
+  favicon: Favicon,
+  loading_icon: loader,
+  project_name: `mattpsvreis' Portfolio`,
+  cover_image: CoverImage,
+};
---
+++ src/contexts/content-theme/content-theme.types.ts
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from 'react';
+
+export type CustomThemeTemplateProps = {
+  theme: {
+    logo: string;
+    light_logo: string;
+    favicon: string;
+    loading_icon: string;
+    project_name: string;
+    cover_image: string;
+    tailwind: any;
+  };
+};
+
+
+export type ContentThemeProviderProps = CustomThemeTemplateProps &
+  PropsWithChildren;
