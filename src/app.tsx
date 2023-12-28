62d3052f01bbb22e9af6521579c8e843bad57760
Mateus Reis <mateuspsvreis@gmail.com>
2023-12-28 15:35:02 -0300

    feat: better nomenclature, adding errorboundary to app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -3,13 +3,16 @@ import resolveConfig from 'tailwindcss/resolveConfig';
 import { ContentThemeProvider } from './contexts/content-theme/content-theme.js';
 import { CONTENT_THEME } from './constants/content-theme.js';
 import { TailwindConfig } from '../tailwind.config.js';
+import { ErrorBoundary } from './infrastructure/errors/error-boundary/error-boundary.js';
 
 
 // @ts-ignore
 const tailwindTheme = resolveConfig(TailwindConfig);
 
 function App() {
-  return <ContentThemeProvider theme={{
+  return (
+    <ErrorBoundary>
+      <ContentThemeProvider theme={{
     logo: CONTENT_THEME.logo,
     light_logo: CONTENT_THEME.light_logo,
     favicon: CONTENT_THEME.favicon,
@@ -17,7 +20,9 @@ function App() {
     project_name: CONTENT_THEME.project_name,
     cover_image: CONTENT_THEME.cover_image,
     tailwind: tailwindTheme,
-  }}></ContentThemeProvider>;
+  }}></ContentThemeProvider>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
