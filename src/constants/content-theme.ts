3e347f2dcc35636a79aafe7965d21715114e9e64
Mateus Reis <mateuspsvreis@gmail.com>
2023-12-28 16:31:48 -0300

    fix: fixed file imports

--- src/constants/content-theme.ts
+++ src/constants/content-theme.ts
@@ -1,5 +1,5 @@
-import loader from './assets/gifs/loader.gif';
-import Favicon from './assets/svgs/favicon.svg';
+import loader from '../assets/gifs/loader.gif';
+import Favicon from '../assets/svgs/favicon.svg';
 
 export const CONTENT_THEME = {
   favicon: Favicon,
