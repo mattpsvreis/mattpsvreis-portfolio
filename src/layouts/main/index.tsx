3157f94a4739e675bf627b82f473f7c5582b140f
Mateus Reis <mateuspsvreis@gmail.com>
2024-01-30 19:23:35 -0300

    feat: some basic layout navbar skeleton

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -6,19 +6,27 @@ interface MainProps {
 
 export const Main: FC<MainProps> = ({ children }) => {
   return (
-    <main id='main' className='flex min-h-full w-full flex-col'>
+    <main id='main' className='flex min-h-screen w-screen flex-col'>
       <div
         id='navbar'
-        className='fixed flex h-32 w-full flex-col justify-between'
+        className='align-center fixed flex h-16 w-full flex-row justify-between px-8'
       >
-        <div id='navbar-item' className='w-1/2'>
-          <h1>Navbar</h1>
+        <div id='navbar-left' className='flex h-full w-1/2 justify-start'>
+          <div id='navbar-item' className='h-full'>
+            <h1 id='navbar-item-text' className='text-center'>
+              Navbar
+            </h1>
+          </div>
         </div>
-        <div id='navbar-item' className='w-1/2'>
-          <h1>Navbar</h1>
+        <div id='navbar-right' className='flex h-full w-1/2 justify-end'>
+          <div id='navbar-item' className='h-full'>
+            <h1 id='navbar-item-text' className='text-center'>
+              Navbar navbar navbar navbar navbar navbar navbar navbar navbar
+            </h1>
+          </div>
         </div>
       </div>
-      <section id='content' className='h-full w-full'>
+      <section id='content' className='mt-16 w-full px-8'>
         {children}
       </section>
     </main>
