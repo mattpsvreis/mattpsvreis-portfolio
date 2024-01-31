1bd687104d7bfca7177ab205effc801c6f67851a
Mateus Reis <mateuspsvreis@gmail.com>
2024-01-31 00:34:24 -0300

    fix: footer no longer on top of blinking arrow

--- src/components/base/footer/index.tsx
+++ src/components/base/footer/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 export const Footer: FC = () => {
   return (
-    <div id='footer' className='fixed bottom-0 w-full px-16 pb-8 text-gray-500'>
+    <div id='footer' className='fixed bottom-0 px-16 pb-8 text-gray-500'>
       <h1 id='copyright-text'>Made by @mattpsvreis | 2024 ©</h1>
     </div>
   );
