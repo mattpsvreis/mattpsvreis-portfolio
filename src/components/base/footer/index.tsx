95d46eede6b3bc0b70e38e0b619e20bf780c6a9d
Mateus Reis <mateuspsvreis@gmail.com>
2024-07-25 14:35:49 -0300

    feat: pirate flag

--- src/components/base/footer/index.tsx
+++ src/components/base/footer/index.tsx
@@ -1,12 +1,21 @@
 import { FC } from 'react';
 
+import pirateflag from '@assets/images/pirate-flag.png';
+
 export const Footer: FC = () => {
   return (
     <div
       id='footer'
-      className='bottom-0 w-full px-8 pb-8 pt-8 text-sm text-indigo-400 xl:fixed xl:pt-0'
+      className='bottom-0 flex w-full flex-row items-center justify-between px-8 pt-8 text-sm text-indigo-400 xl:fixed xl:items-end xl:pt-0'
     >
-      <h1 id='copyright-text'>Made by @mattpsvreis | 2024 ©</h1>
+      <h1 id='copyright-text' className='pb-8'>
+        Made by @mattpsvreis | 2024 ©
+      </h1>
+      <img
+        src={pirateflag}
+        alt=''
+        className='h-16 xl:h-32 4xl:-mb-7 4xl:h-48'
+      />
     </div>
   );
 };
