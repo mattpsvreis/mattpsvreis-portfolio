f57e491653b23049c042e19d343898ee9b6766b5
Mateus Reis <mateuspsvreis@gmail.com>
2024-07-25 14:44:15 -0300

    fix: bottom margin for pirate flag on different screens other than 2k

--- src/components/base/footer/index.tsx
+++ src/components/base/footer/index.tsx
@@ -8,13 +8,13 @@ export const Footer: FC = () => {
       id='footer'
       className='bottom-0 flex w-full flex-row items-center justify-between px-8 pt-8 text-sm text-indigo-400 xl:fixed xl:items-end xl:pt-0'
     >
-      <h1 id='copyright-text' className='pb-8'>
+      <h1 id='copyright-text' className='xl:pb-8'>
         Made by @mattpsvreis | 2024 ©
       </h1>
       <img
         src={pirateflag}
         alt=''
-        className='h-16 xl:h-32 4xl:-mb-7 4xl:h-48'
+        className='h-16 xl:-mb-4 xl:h-32 4xl:-mb-7 4xl:h-48'
       />
     </div>
   );
